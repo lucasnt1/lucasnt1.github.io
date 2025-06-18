07b66f9cd636e1ba9690266c95f3b008ccb3cc80
lucasnt1 <86374198+lucasnt1@users.noreply.github.com>
2025-06-18 11:12:30 +0200

    Add files via upload

---
+++ senden.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$message = $_POST['message'] ?? '';
+
+$to = "deine@email.de"; // Empfängeradresse
+$subject = "Neue Nachricht von $name";
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
+
+$body = "Name: $name\nE-Mail: $email\n\nNachricht:\n$message";
+
+if (mail($to, $subject, $body, $headers)) {
+  echo "E-Mail erfolgreich gesendet!";
+} else {
+  echo "Fehler beim Senden der E-Mail.";
+}
+?>
